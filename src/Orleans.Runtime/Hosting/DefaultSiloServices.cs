669bd5905b7cfad836da432bdb6548cffb4783a7
Koen <koen@linker.io>
2024-08-26 02:59:17 +0100

    Implemented runtime

--- src/Orleans.Runtime/Hosting/DefaultSiloServices.cs
+++ src/Orleans.Runtime/Hosting/DefaultSiloServices.cs
@@ -43,6 +43,8 @@ using Microsoft.Extensions.Configuration;
 using Orleans.Serialization.Internal;
 using Orleans.Core;
 using Orleans.Placement.Repartitioning;
+using Orleans.Runtime.Cancellation;
+using Orleans.Serialization.Invocation;
 
 namespace Orleans.Hosting
 {
@@ -99,6 +101,9 @@ namespace Orleans.Hosting
             services.TryAddSingleton<IGrainCancellationTokenRuntime, GrainCancellationTokenRuntime>();
             services.AddTransient<CancellationSourcesExtension>();
             services.AddKeyedTransient<IGrainExtension>(typeof(ICancellationSourcesExtension), (sp, _) => sp.GetRequiredService<CancellationSourcesExtension>());
+            services.TryAddTransient<ICancellationRuntime, CancellationRuntime>();
+            services.AddTransient<CancellableInvokableGrainExtension>();
+            services.AddKeyedTransient<IGrainExtension>(typeof(ICancellableInvokableGrainExtension), (sp, _) => sp.GetRequiredService<CancellableInvokableGrainExtension>());
             services.TryAddSingleton<GrainFactory>(sp => sp.GetRequiredService<InsideRuntimeClient>().ConcreteGrainFactory);
             services.TryAddSingleton<InterfaceToImplementationMappingCache>();
             services.TryAddSingleton<GrainInterfaceTypeToGrainTypeResolver>();
